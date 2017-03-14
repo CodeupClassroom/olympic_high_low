6d57d84cb71b49585df09e6121c9726ae4fba7ca
Luis Montealegre <montealegreluis@gmail.com>
2017-03-14 11:04:40 -0500

    Completed the case when the user guess at the first attempt.

--- high_low.php
+++ high_low.php
@@ -1,10 +1,14 @@
 <?php
 // input
 // user guess
+$random = 10;
+$guess = 10;
 
 // process
+if ($guess == $random) {
+    $message = 'GUESS!';
+}
 
 // output
 // message that can be higher, lower, guess!
-
-
+fwrite(STDOUT, $message . PHP_EOL);
