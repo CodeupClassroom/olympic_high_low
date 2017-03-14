ec366bb391cf87e1fc3120dc112fd65b1fe88a08
Luis Montealegre <montealegreluis@gmail.com>
2017-03-14 11:06:42 -0500

    Completed the case when the user guess is less than the random value.

--- high_low.php
+++ high_low.php
@@ -2,11 +2,13 @@
 // input
 // user guess
 $random = 10;
-$guess = 10;
+$guess = 8;
 
 // process
 if ($guess == $random) {
     $message = 'GUESS!';
+} elseif ($guess < $random) {
+    $message = 'HIGHER';
 }
 
 // output
