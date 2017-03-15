6c61953ca6af340c3e3251293a23b12f3d3eb150
Luis Montealegre <montealegreluis@gmail.com>
2017-03-15 09:59:52 -0500

    Use a while loop instead of a for loop.

--- for.php
+++ for.php
@@ -6,11 +6,11 @@ $start = trim(fgets(STDIN));
 fwrite(STDOUT, 'Enter the end of the range: ');
 $end = trim(fgets(STDIN));
 
-/*initialization*/
-for ($i = $start; $i <= $end ;$i++) {
-    //  process: modulus
+$i = $start;
+while ($i <= $end) {
     if ($i % 2 === 0) {
         // output: only even numbers
         echo $i, PHP_EOL;
     }
+    $i++;
 }
