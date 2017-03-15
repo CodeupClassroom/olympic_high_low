f362d64d1d02118c183e452e1ed596450817044c
Luis Montealegre <montealegreluis@gmail.com>
2017-03-15 09:21:59 -0500

    Add validation for the CLI arguments.

--- high_low.php
+++ high_low.php
@@ -1,11 +1,43 @@
 <?php
-// input
-// user guess
-$random = mt_rand(1, 100);
+if ($argc == 3) {
+    // make sure the arguments are numbers
+    if (is_numeric($argv[1]) && is_numeric($argv[2])) {
+        $min = $argv[1];
+        $max = $argv[2];
+    } else {
+        // prompt the user for a numeric minimum until they provide a numeric minimum
+        do {
+            fwrite(STDOUT, "Please input a minimum number. Input must be numeric" . PHP_EOL);
+            $min = trim(fgets(STDIN));
+        } while(!is_numeric($min));
+
+        // prompt for a maximum that is numeric
+        do {
+            fwrite(STDOUT, "Please input a maximum number. Input must be numeric" . PHP_EOL);
+            $max = trim(fgets(STDIN));
+        } while(!is_numeric($max));
+    }
+} else {
+    $min = 1;
+    $max = 100;
+}
+
+// if min > max, swap them
+if ($min > $max) {
+    $min = $argv[2];
+    $max = $argv[1];
+}
+
+$random = mt_rand($min, $max);
 
 do {
-    fwrite(STDOUT, 'Guess the number' . PHP_EOL);
-    $guess = trim(fgets(STDIN));
+    // input
+    // user guess
+    // refactor this to a function called getNumber()
+    do {
+        fwrite(STDOUT, "Please guess a number between $min and $max" . PHP_EOL);
+        $guess = trim(fgets(STDIN));
+    } while (!is_numeric($guess));
 
     // process
     if ($guess == $random) {
