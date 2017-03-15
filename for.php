bdc0940f60347f72835b933f79be4dfba26a2eb7
Ryan Orsinger <ryan.orsinger@gmail.com>
2017-03-15 15:47:21 -0500

    Add ability to take in command line arguments and make sure they're numeric. Default to prompting the user if command line args are invalid.

--- for.php
+++ for.php
@@ -1,15 +1,23 @@
 <?php
 
-do {
-	// input: starting and a ending value
-	fwrite(STDOUT, 'Enter a starting number' . PHP_EOL);
-	$starting = trim(fgets(STDIN));
-} while(!is_numeric($starting));
+// take in command line arguments for starting and ending number
+if(isset($argv[1], $argv[2]) && (is_numeric($argv[1]) && is_numeric($argv[2]))) {
+	$starting = $argv[1];
+	$ending = $argv[2];
+} else {
 
-do {
-	fwrite(STDOUT, 'Enter an ending number' . PHP_EOL);
-	$ending = trim(fgets(STDIN));
-} while(!is_numeric($ending));
+	do {
+		// input: starting and a ending value
+		fwrite(STDOUT, 'Enter a starting number' . PHP_EOL);
+		$starting = trim(fgets(STDIN));
+	} while(!is_numeric($starting));
+
+	do {
+		fwrite(STDOUT, 'Enter an ending number' . PHP_EOL);
+		$ending = trim(fgets(STDIN));
+	} while(!is_numeric($ending));
+
+}
 
 fwrite(STDOUT, 'Enter an increment value' . PHP_EOL);
 $increment = trim(fgets(STDIN));
