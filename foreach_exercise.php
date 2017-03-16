6b5a2240f744027812c947f804614a734d0418af
Luis Montealegre <montealegreluis@gmail.com>
2017-03-16 10:24:56 -0500

    Solve the foreach exercise.

---
+++ foreach_exercise.php
@@ -0,0 +1,23 @@
+<?php
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+
+//  PHP: count($things)
+// JS: $things.length
+
+/*for ($i = 0; $i < count($things); $i++) {
+    $thing = $things[$i];
+
+}*/
+
+foreach ($things as $thing) {
+    if (is_array($thing)) {
+        echo 'Array ( ';
+        foreach ($thing as $innerThing) {
+            // echo = language construct
+            echo $innerThing, ' ';
+        }
+        echo ')', PHP_EOL;
+    } else {
+        echo $thing, PHP_EOL;
+    }
+}
